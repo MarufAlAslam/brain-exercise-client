85f267b3fa348b0e556dc10e886bcc76ffcf2ff0
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-10-25 10:13:53 +0600

    react router installed

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <h1 className='text-2xl'>Main</h1>
         </div>
     );
